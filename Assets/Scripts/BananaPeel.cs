b95d52add7af48cce487c7de643c604884c52f0c
SpazzyKidd <humanlydemonic@gmail.com>
2018-12-01 15:31:26 -0600

    Drag banana peel

---
+++ Assets/Scripts/BananaPeel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BananaPeel : MonoBehaviour {
+
+    private float mouseX;
+
+    private void OnMouseDown()
+    {
+        mouseX = Input.mousePosition.x;
+    }
+
+    private void OnMouseDrag()
+    {
+        float newMouseX = Input.mousePosition.x;
+        float mouseXChange = newMouseX - mouseX;
+
+        transform.Translate(Vector3.right * Time.deltaTime * mouseXChange);
+
+        mouseX = newMouseX;
+    }
+}
--- Assets/Scripts/JamieWeld.cs
+++ Assets/Scripts/JamieWeld.cs
@@ -4,17 +4,9 @@ using UnityEngine;
 
 public class JamieWeld : MonoBehaviour {
 
-
     public float speed = 0;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
 	void Update () {
-
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
 	}
 }
