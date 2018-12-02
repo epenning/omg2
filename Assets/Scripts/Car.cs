562dd2cad4b76c9c4c9e05bc8fb72b69f72804e2
SpazzyKidd <humanlydemonic@gmail.com>
2018-12-01 20:48:21 -0600

    Added moving car on asphalt road

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour {
+
+    public float speed = 0;
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * GameTime.deltaTime * speed);
+    }
+}
