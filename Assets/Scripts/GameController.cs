55d540baf0d1a91a69b3e9511baf6dafdddf577f
SpazzyKidd <humanlydemonic@gmail.com>
2018-12-01 20:00:44 -0600

    Initial pause menu (not quite working)

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,8 +5,45 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 
-	public void StartGame()
+    private bool paused = false;
+
+    public GameObject pauseMenu;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && SceneManager.GetActiveScene().buildIndex != 0)
+        {
+            if (paused)
+            {
+                ContinueGame();
+            } else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PauseGame()
+    {
+        paused = true;
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+    }
+
+    public void ContinueGame()
+    {
+        paused = false;
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
