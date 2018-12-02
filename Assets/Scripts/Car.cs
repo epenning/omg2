90c76c1462ec84af273a09c7dda935af82fda1b8
SpazzyKidd <humanlydemonic@gmail.com>
2018-12-01 21:08:23 -0600

    Clicking on car stops it with deceleration

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -5,9 +5,26 @@ using UnityEngine;
 public class Car : MonoBehaviour {
 
     public float speed = 0;
+    public float deceleration = 0;
 
     void Update()
     {
+        if (Mathf.Abs(speed) < deceleration)
+        {
+            speed = 0;
+        } else if (speed > 0)
+        {
+            speed -= deceleration;
+        } else if (speed < 0)
+        {
+            speed += deceleration;
+        }
+
         transform.Translate(Vector3.right * GameTime.deltaTime * speed);
+    }
+
+    private void OnMouseDown()
+    {
+        deceleration = 0.1f;
     }
 }
