a4c4b23755841ba16b0d45369edcb3407cd6a95a
Erin Pennington <epennington10@gmail.com>
2018-12-02 18:28:07 -0600

    coded tree to fall and kill player.

--- Assets/Scripts/TreeKill.cs
+++ Assets/Scripts/TreeKill.cs
@@ -4,8 +4,37 @@ using UnityEngine;
 
 public class TreeKill : MonoBehaviour {
 
+    public float fallSpeed = 10;
+    public int direction = 1;
+
+    private bool falling;
+    private bool fallen;
+
+    private void Update()
+    {
+        if (falling)
+        {
+            Vector3 rotation = transform.eulerAngles;
+            rotation.z += fallSpeed * GameTime.deltaTime * direction;
+            transform.eulerAngles = rotation;
+
+            if (transform.eulerAngles.z > 90 && transform.eulerAngles.z < 270)
+            {
+                falling = false;
+                fallen = true;
+
+                rotation = transform.eulerAngles;
+                rotation.z = 90 * direction;
+                transform.eulerAngles = rotation;
+            }
+        }
+    }
+
     private void OnMouseDown()
     {
-        
+        if (!fallen)
+        {
+            falling = true;
+        }
     }
 }
