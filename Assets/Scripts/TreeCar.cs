5798c72f4200f6f6f6ebcf7c3f712797ffcfd4a8
Erin Pennington <epennington10@gmail.com>
2018-12-02 18:41:10 -0600

    Tree car falls forward

--- Assets/Scripts/TreeCar.cs
+++ Assets/Scripts/TreeCar.cs
@@ -2,10 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TreeCar : MonoBehaviour {
+public class TreeCar : MonoBehaviour
+{
+
+    public float fallSpeed = 10;
+
+    private bool falling;
+    private bool fallen;
+
+    private void Update()
+    {
+        if (falling)
+        {
+            Vector3 rotation = transform.eulerAngles;
+            rotation.x += fallSpeed * GameTime.deltaTime;
+            transform.eulerAngles = rotation;
+
+            if (transform.eulerAngles.x > 90)
+            {
+                falling = false;
+                fallen = true;
+
+                rotation = transform.eulerAngles;
+                rotation.x = 90;
+                transform.eulerAngles = rotation;
+            }
+        }
+    }
 
     private void OnMouseDown()
     {
-        
+        if (!fallen)
+        {
+            falling = true;
+        }
     }
 }
