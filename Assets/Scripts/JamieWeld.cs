7a66c0966ca2329331e15e3d4de5131c4f08b6fe
Erin Pennington <epennington10@gmail.com>
2018-12-02 20:20:09 -0600

    Animate player to run by flipping sprite

--- Assets/Scripts/JamieWeld.cs
+++ Assets/Scripts/JamieWeld.cs
@@ -5,8 +5,24 @@ using UnityEngine;
 public class JamieWeld : MonoBehaviour {
 
     public float speed = 0;
-	
-	void Update () {
+    public int framesPerFlip = 10;
+
+    private SpriteRenderer sprite;
+    private int framesSinceFlip = 0;
+
+    private void Start()
+    {
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    void Update () {
         transform.Translate(Vector3.forward * GameTime.deltaTime * speed);
-	}
+
+        framesSinceFlip++;
+        if (framesSinceFlip > framesPerFlip)
+        {
+            framesSinceFlip = 0;
+            sprite.flipX = !sprite.flipX;
+        }
+    }
 }
